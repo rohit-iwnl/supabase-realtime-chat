193882cdd443dce5ccb158df62992cbb8a2db311
rohit-iwnl <rohitmanivel9@gmail.com>
2024-04-06 01:22:54 -0700

    add zustand state manager and add initUser

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/ui/Header";
+import InitUser from "@/lib/store/initUser";
 import { createClient} from "@/utils/supabase/server";
 import React from "react";
 
@@ -11,10 +12,13 @@ export default async function Page() {
   console.log(data?.user)
 
   return (
+    <>
     <div className="max-w-3xl mx-auto md:py-10 h-screen">
       <div className="h-full border rounded-md">
         <Header user={data?.user}/>
       </div>
     </div>
+    <InitUser user={data?.user} />
+    </>
   );
 }
